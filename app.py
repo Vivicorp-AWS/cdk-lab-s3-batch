e0e4cfd678119360dffa498d6dbd060db05a01bf
Violet Vivirand <purpleslovemail@gmail.com>
2023-11-27 09:41:25 +0800

    Remove unnecessary stacks

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ import aws_cdk as cdk
 from stacks.top_stack import TopStack
 from stacks.ecr_stack import ECRStack
 from stacks.batchcompute_stack import BatchComputeStack
-from stacks.s3_stack import S3Stack
-from stacks.batch_stack import BatchStack
-from stacks.lambda_stack import LambdaStack
 
 app = cdk.App()
 
@@ -22,8 +19,6 @@ ecr_stack = ECRStack(
 )
 
 docker_image_asset = ecr_stack.docker_image_asset
-# [TODO] ?
-# self.docker_image.repository.grantPull(principal)
 
 batchcompute_stack = BatchComputeStack(
     top_stack, f"batchcompute-stack",
@@ -31,35 +26,6 @@ batchcompute_stack = BatchComputeStack(
     docker_image_asset=docker_image_asset,
 )
 
-# s3_stack = S3Stack(
-#     top_stack, f"s3-stack",
-#     description="CDK S3 & Batch Lab S3 Stack",
-# )
-# bucket_source = s3_stack.bucket_source
-# bucket_destination = s3_stack.bucket_destination
-# bucket_destination_name = bucket_destination.bucket_name
-
-# batch_stack = BatchStack(
-#     top_stack, f"batch-stack",
-#     description="CDK S3 & Batch Lab Batch Stack",
-#     docker_image_asset=docker_image_asset,
-#     bucket_source=bucket_source,
-#     bucket_destination=bucket_destination,
-# )
-# job_queue_arn = batch_stack.job_queue.job_queue_arn
-# job_definition_arn = batch_stack.job_definition.job_definition_arn
-
-# lambda_stack = LambdaStack(
-#     top_stack, f"cdklab-lambda-stack",
-#     description="CDK S3 & Batch Lab Lambda Stack",
-#     job_queue_arn=job_queue_arn,
-#     job_definition_arn=job_definition_arn,
-#     bucket_destination_name=bucket_destination_name,
-#     bucket_source=bucket_source,
-# )
-
-# batch_stack.add_dependency(s3_stack)
-# batch_stack.add_dependency(ecr_stack)
-# lambda_stack.add_dependency(batch_stack)
+batchcompute_stack.add_dependency(ecr_stack)
 
 app.synth()
